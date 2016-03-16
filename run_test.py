18a89b0ed3de5d7b24929cb5e91b38f90ed052ff
SmellyCat <smellycat2014@foxmail.com>
2016-03-16 23:09:59 +0800

    删除 无用的测试文件

--- run_test.py
+++
@@ -1,8 +0,0 @@
-from app import app, views
-#from ini_conf import MyIni
-
-if __name__ == '__main__':
-##    my_ini = MyIni('my_ini.conf')
-##    s_ini = my_ini.get_sys()
-##    h_ini = my_ini.get_hzhbc()
-    app.run(port=8098, threaded=True)
